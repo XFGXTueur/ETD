4ae043b709910e0cf9d23645e7251bac64c02783
Flament Thomas <waltorias.as@gmail.com>
2022-11-23 11:47:34 +0100

    feat: added logo and loader animation

--- src/Loader/Loader.jsx
+++ src/Loader/Loader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loader = () => {
     return (
         <div className="loader__container">
-            <img className="loader__container" src="" alt="loader"></img>
+            <img className="loader__container-img" src="./Images/logo.webp" alt="loader"></img>
         </div>
     );
 }
