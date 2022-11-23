b45b6053229757c108ae4c29a4ff4c22d794eb99
Flament Thomas <waltorias.as@gmail.com>
2022-11-23 10:44:45 +0100

    feat structure and base features

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import './App.css';
+import React, {useEffect, useState} from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom"; // Ajout de la dépendance react-router-dom pour créer des routes
 
-function App() {
+import Loader from "./Loader/Loader";
+import Home from "./Pages/Home/Home";
+import Devis from "./Pages/Devis/Devis";
+import Factures from "./Pages/Factures/Factures";
+
+const App = () => {
+  // Création d'un état pour le loader
+  const [loading, setLoading] = useState(false);
+
+  // useEffect pour simuler un chargement
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+        setLoading(false);
+    }, 1500);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>  
+    <>
+    {/* Si le chargement est en cours, on affiche le loader sinon on affiche les routes/pages */}
+      {loading ? (
+          <div className="loader__container">
+              <Loader/>
+          </div>
+      ) : (
+          <BrowserRouter>
+              <Routes>
+                  <Route path="/" element={ <Home/> } />
+                  <Route path="/devis" element={ <Devis/> } />
+                  <Route path="/factures" element={ <Factures/> } />
+              </Routes>
+          </BrowserRouter>
+      )}
+    </>
   );
 }
 
---
+++ src/Pages/Devis/Devis.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Devis = () => {
+    return (
+        <div className="devis">
+            <div className="devis__container">
+                
+            </div>
+        </div>
+    );
+}
+
+export default Devis;
