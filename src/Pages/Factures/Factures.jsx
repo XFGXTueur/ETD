38e56520fc7cedd91af227b086bb443709654c51
XFGX Tueur <89538048+XFGXTueur@users.noreply.github.com>
2022-11-23 14:52:51 +0100

    feat: Home Style (#3)
    * feat: Home Style
    
    * feat: Home Style

--- src/Pages/Factures/Factures.jsx
+++ src/Pages/Factures/Factures.jsx
@@ -4,7 +4,9 @@ const Factures = () => {
     return (
         <div className="factures">
             <div className="factures__container">
-                
+                <div className="factures__container-btn">
+                    <a href="/" className="factures__container-btn-link" alt="home">Home</a>
+                </div>
             </div>
         </div>
     );
