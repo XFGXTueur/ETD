1dce1a826e308983e96217a164fcf8a6f015b759
Thomas F <68472970+WalTho@users.noreply.github.com>
2022-11-23 12:24:25 +0100

    feat structure and base features (#2)
    * feat structure and base features
    
    * refactor: deleted unused files
    
    * feat: set ETD app name
    
    * feat: added logo and loader animation

---
+++ src/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loader = () => {
+    return (
+        <div className="loader__container">
+            <img className="loader__container-img" src="./Images/logo.webp" alt="loader"></img>
+        </div>
+    );
+}
+
+export default Loader;
---
+++ src/Pages/Factures/Factures.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Factures = () => {
+    return (
+        <div className="factures">
+            <div className="factures__container">
+                
+            </div>
+        </div>
+    );
+}
+
+export default Factures;
