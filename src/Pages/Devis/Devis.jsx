a17a4b0f84f1d9049caf06ff25a0e83d820d7926
XFGX Tueur <89538048+XFGXTueur@users.noreply.github.com>
2022-11-25 21:25:26 +0100

    feat: Section class added (#15)

--- src/Pages/Devis/Devis.jsx
+++ src/Pages/Devis/Devis.jsx
@@ -35,172 +35,175 @@ const Devis = () => {
             </button>
             <div className="devis__container" ref={componentRef}>
                 {/*  Premier Bloc  */}
-                <div className="devis__container-first-block">
-                    <a href="/" className="devis__container-first-block-link" alt="home">
-                        <img
-                            className="devis__container-first-block-link-img"
-                            alt="logo"
-                            src="./logo512.png"
-                        ></img>
-                    </a>
+                <a href="/" className="devis__container-link" alt="home">
+                    <img
+                        className="devis__container-link-img"
+                        alt="logo"
+                        id="img"
+                        src="./logo512.png"
+                    ></img>
+                </a>
 
-                    <div className="devis__container-first-block-infos">
-                        <div className="devis__container-first-block-infos-devis">
-                            <p className="devis__container-first-block-infos-devis-text">DEVIS :</p>
+                <div className="devis__container-section">
+                    <div className="devis__container-section-first-block">
+                        <div className="devis__container-section-first-block-infos">
+                            <div className="devis__container-section-first-block-infos-devis">
+                                <p className="devis__container-section-first-block-infos-devis-text">DEVIS :</p>
+                                <input
+                                    className="devis__container-section-first-block-infos-devis-input"
+                                    type="text"
+                                />
+                            </div>
+                            <div className="devis__container-section-first-block-infos-date">
+                                <p className="devis__container-section-first-block-infos-date-text">Date :</p>
+                                <input
+                                    className="devis__container-section-first-block-infos-date-input"
+                                    type="date"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    {/*  Second Bloc  */}
+                    <div className="devis__container-section-second-block">
+                        <div className="devis__container-section-second-block-me">
+                            <p className="devis__container-section-second-block-me-text">
+                                Ma Boite
+                            </p>
+                            <p className="devis__container-section-second-block-me-text">
+                                Adresse, 0 rue Nom
+                            </p>
+                            <p className="devis__container-section-second-block-me-text">
+                                Ma Ville
+                            </p>
+                            <p className="devis__container-section-second-block-me-text">
+                                Mon Pays
+                            </p>
+                            <p className="devis__container-section-second-block-me-text">
+                                mail@mail.com
+                            </p>
+                            <p className="devis__container-section-second-block-me-text">
+                                Tél: 06 06 06 06 06
+                            </p>
+                        </div>
+                        <div className="devis__container-section-second-block-client">
                             <input
-                                className="devis__container-first-block-infos-devis-input"
+                                className="devis__container-section-second-block-client-input-name"
                                 type="text"
                             />
-                        </div>
-                        <div className="devis__container-first-block-infos-date">
-                            <p className="devis__container-first-block-infos-date-text">Date :</p>
-                            <input
-                                className="devis__container-first-block-infos-date-input"
-                                type="date"
-                            />
+                            <textarea
+                                className="devis__container-section-second-block-client-input-address"
+                            ></textarea>
                         </div>
                     </div>
-                </div>
-                {/*  Second Bloc  */}
-                <div className="devis__container-second-block">
-                    <div className="devis__container-second-block-me">
-                        <p className="devis__container-second-block-me-text">
-                            Ma Boite
+                    {/*  Troisieme Bloc  */}
+                    <div className="devis__container-section-third-block">
+                        <p className="devis__container-section-third-block-title">
+                            Devis
                         </p>
-                        <p className="devis__container-second-block-me-text">
-                            Adresse, 0 rue Nom
-                        </p>
-                        <p className="devis__container-second-block-me-text">
-                            Ma Ville
-                        </p>
-                        <p className="devis__container-second-block-me-text">
-                            Mon Pays
-                        </p>
-                        <p className="devis__container-second-block-me-text">
-                            mail@mail.com
-                        </p>
-                        <p className="devis__container-second-block-me-text">
-                            Tél: 06 06 06 06 06
-                        </p>
-                    </div>
-                    <div className="devis__container-second-block-client">
-                        <input
-                            className="devis__container-second-block-client-input-name"
-                            type="text"
-                        />
                         <textarea
-                            className="devis__container-second-block-client-input-address"
+                            className="devis__container-section-third-block-input"
+                            type="text"
                         ></textarea>
                     </div>
-                </div>
-                {/*  Troisieme Bloc  */}
-                <div className="devis__container-third-block">
-                    <p className="devis__container-third-block-title">
-                        Devis
-                    </p>
-                    <textarea
-                        className="devis__container-third-block-input"
-                        type="text"
-                    ></textarea>
-                </div>
-                {/*  Quatrieme Bloc  */}
-                <div className="devis__container-fourth-block">
-                    <table className="devis__container-fourth-block-table">
-                        <thead className="devis__container-fourth-block-table-head">
-                            <td className="devis__container-fourth-block-table-head-description">
-                                Description
-                            </td>
-                            <td className="devis__container-fourth-block-table-head-qty">
-                                Quantité
-                            </td>
-                            <td className="devis__container-fourth-block-table-head-unit">
-                                Unité
-                            </td>
-                            <td className="devis__container-fourth-block-table-head-unitPrice">
-                                Prix unitaire
-                            </td>
-                            <td className="devis__container-fourth-block-table-head-tva">
-                                % TVA
-                            </td>
-                            <td className="devis__container-fourth-block-table-head-total-tva">
-                                Total TVA
-                            </td>
-                            <td className="devis__container-fourth-block-table-head-total">
-                                Total
-                            </td>
-                        </thead>
-                        <tbody className="devis__container-fourth-block-table-body">
-                        {[...Array(noOfRows)].map((elementInArray, index) => {
-                                return (
-                                    <Item key={16}/>
-                                );
-                            })}
-                        </tbody>
-                    </table>
-                </div>
-                {/*  Quatrieme Bloc BIS */}
-                <div className="devis__container-fourthBIS-block">
-                    <table className="devis__container-fourthBIS-block-table">
-                        <thead className="devis__container-fourthBIS-block-table-head">
-                            <td className="devis__container-fourthBIS-block-table-head-tva">
-                                <p className="devis__container-fourthBIS-block-table-head-tva-text">
+                    {/*  Quatrieme Bloc  */}
+                    <div className="devis__container-section-fourth-block">
+                        <table className="devis__container-section-fourth-block-table">
+                            <thead className="devis__container-section-fourth-block-table-head">
+                                <td className="devis__container-section-fourth-block-table-head-description">
+                                    Description
+                                </td>
+                                <td className="devis__container-section-fourth-block-table-head-qty">
+                                    Quantité
+                                </td>
+                                <td className="devis__container-section-fourth-block-table-head-unit">
+                                    Unité
+                                </td>
+                                <td className="devis__container-section-fourth-block-table-head-unitPrice">
+                                    Prix unitaire
+                                </td>
+                                <td className="devis__container-section-fourth-block-table-head-tva">
+                                    % TVA
+                                </td>
+                                <td className="devis__container-section-fourth-block-table-head-total-tva">
                                     Total TVA
-                                </p>
-                                <input className="devis__container-fourthBIS-block-table-head-tva-value" type="text" readOnly="readonly"/>
-                            </td>
-                            <td className="devis__container-fourthBIS-block-table-head-totalDF">
-                                <p className="devis__container-fourthBIS-block-table-head-totalDF-text">
-                                    Total HT
-                                </p>
-                                <input className="devis__container-fourthBIS-block-table-head-totalDF-value" type="number" readOnly="readonly"/>
-                            </td>
-                            <td className="devis__container-fourthBIS-block-table-head-total">
-                                <p className="devis__container-fourthBIS-block-table-head-total-text">
+                                </td>
+                                <td className="devis__container-section-fourth-block-table-head-total">
                                     Total
-                                </p>
-                                <input className="devis__container-fourthBIS-block-table-head-total-value" type="number" readOnly="readonly"/>
-                            </td>
-                        </thead>
-                    </table>
-                </div>
-                {/*  Cinquieme Bloc  */}
-                <div className="devis__container-fifth-block">
-                    <p className="devis__container-fifth-block-title">
-                        TITRE CGV
-                    </p>
-                    <p className="devis__container-fifth-block-cgv">
-                        CGV
-                    </p>
-                </div>
-                {/*  Sixieme Bloc  */}
-                <div className="devis__container-sixth-block">
-                    <div className="devis__container-sixth-block-sign">
-                        <img
-                            className="devis__container-sixth-block-sign-img"
-                            alt="signature"
-                            href=""
-                        ></img>
+                                </td>
+                            </thead>
+                            <tbody className="devis__container-section-fourth-block-table-body">
+                            {[...Array(noOfRows)].map((elementInArray, index) => {
+                                    return (
+                                        <Item key={16}/>
+                                    );
+                                })}
+                            </tbody>
+                        </table>
                     </div>
-                    <div className="devis__container-sixth-block-sign">
-                        <p className="devis__container-sixth-block-sign-client">
-                            Ma signature :
+                    {/*  Quatrieme Bloc BIS */}
+                    <div className="devis__container-section-fourthBIS-block">
+                        <table className="devis__container-section-fourthBIS-block-table">
+                            <thead className="devis__container-section-fourthBIS-block-table-head">
+                                <td className="devis__container-section-fourthBIS-block-table-head-tva">
+                                    <p className="devis__container-section-fourthBIS-block-table-head-tva-text">
+                                        Total TVA
+                                    </p>
+                                    <input className="devis__container-section-fourthBIS-block-table-head-tva-value" type="text" readOnly="readonly"/>
+                                </td>
+                                <td className="devis__container-section-fourthBIS-block-table-head-totalDF">
+                                    <p className="devis__container-section-fourthBIS-block-table-head-totalDF-text">
+                                        Total HT
+                                    </p>
+                                    <input className="devis__container-section-fourthBIS-block-table-head-totalDF-value" type="number" readOnly="readonly"/>
+                                </td>
+                                <td className="devis__container-section-fourthBIS-block-table-head-total">
+                                    <p className="devis__container-section-fourthBIS-block-table-head-total-text">
+                                        Total
+                                    </p>
+                                    <input className="devis__container-section-fourthBIS-block-table-head-total-value" type="number" readOnly="readonly"/>
+                                </td>
+                            </thead>
+                        </table>
+                    </div>
+                    {/*  Cinquieme Bloc  */}
+                    <div className="devis__container-section-fifth-block">
+                        <p className="devis__container-section-fifth-block-title">
+                            TITRE CGV
+                        </p>
+                        <p className="devis__container-section-fifth-block-cgv">
+                            CGV
                         </p>
                     </div>
-                </div>
-                {/*  Septieme Bloc  */}
-                <div className="devis__container-seventh-block">
-                    <p className="devis__container-seventh-block-info-status">
-                        Statut
-                    </p>
-                    <p className="devis__container-seventh-block-info-name">
-                        Ma Boite
-                    </p>
-                    <p className="devis__container-seventh-block-info-bce">
-                        734820078
-                    </p>
-                    <p className="devis__container-seventh-block-info-tva">
-                        734820078
-                    </p>
+                    {/*  Sixieme Bloc  */}
+                    <div className="devis__container-section-sixth-block">
+                        <div className="devis__container-section-sixth-block-sign">
+                            <img
+                                className="devis__container-section-sixth-block-sign-img"
+                                alt="signature"
+                                href=""
+                            ></img>
+                        </div>
+                        <div className="devis__container-section-sixth-block-sign">
+                            <p className="devis__container-section-sixth-block-sign-client">
+                                Ma signature :
+                            </p>
+                        </div>
+                    </div>
+                    {/*  Septieme Bloc  */}
+                    <div className="devis__container-section-seventh-block">
+                        <p className="devis__container-section-seventh-block-info-status">
+                            Statut
+                        </p>
+                        <p className="devis__container-section-seventh-block-info-name">
+                            Ma Boite
+                        </p>
+                        <p className="devis__container-section-seventh-block-info-bce">
+                            734820078
+                        </p>
+                        <p className="devis__container-section-seventh-block-info-tva">
+                            734820078
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/Pages/Devis/Item.jsx
+++ src/Pages/Devis/Item.jsx
@@ -3,33 +3,33 @@ import React from "react";
 const Item = () => {
     return (
         <>
-            <tr className="devis__container-fourth-block-table-body-line" key={0} >
-                <td className="devis__container-fourth-block-table-body-line-description" key={1}>
-                    <textarea className="devis__container-fourth-block-table-body-line-description-input" key={2}></textarea>
+            <tr className="devis__container-section-fourth-block-table-body-line" key={0} >
+                <td className="devis__container-section-fourth-block-table-body-line-description" key={1}>
+                    <textarea className="devis__container-section-fourth-block-table-body-line-description-input" key={2}></textarea>
                 </td>
-                <td className="devis__container-fourth-block-table-body-line-qty" key={3}>
-                    <input className="devis__container-fourth-block-table-body-line-qty-input" type="number" key={4}/>
+                <td className="devis__container-section-fourth-block-table-body-line-qty" key={3}>
+                    <input className="devis__container-section-fourth-block-table-body-line-qty-input" type="number" key={4}/>
                 </td>
-                <td className="devis__container-fourth-block-table-body-line-unit" key={5}>
-                    <input className="devis__container-fourth-block-table-body-line-unit-input" type="text" key={6}/>
+                <td className="devis__container-section-fourth-block-table-body-line-unit" key={5}>
+                    <input className="devis__container-section-fourth-block-table-body-line-unit-input" type="text" key={6}/>
                 </td>
-                <td className="devis__container-fourth-block-table-body-line-unitPrice" key={7}>
-                    <input className="devis__container-fourth-block-table-body-line-unitPrice-input" type="number" key={8}/>
+                <td className="devis__container-section-fourth-block-table-body-line-unitPrice" key={7}>
+                    <input className="devis__container-section-fourth-block-table-body-line-unitPrice-input" type="number" key={8}/>
                 </td>
-                <td className="devis__container-fourth-block-table-body-line-tva" key={9}>
-                    <p className="devis__container-fourth-block-table-body-line-tva-text" type="number" readOnly="readonly" key={10}/>
-                    <select className="devis__container-fourth-block-table-body-line-tva-value" name="tva" id="tva" key={11}>
+                <td className="devis__container-section-fourth-block-table-body-line-tva" key={9}>
+                    <p className="devis__container-section-fourth-block-table-body-line-tva-text" type="number" readOnly="readonly" key={10}/>
+                    <select className="devis__container-section-fourth-block-table-body-line-tva-value" name="tva" id="tva" key={11}>
                             <option value="0">0%</option>
                             <option value="6">6%</option>
                             <option value="12">12%</option>
                             <option value="21">21%</option>
                     </select>
                 </td>
-                <td className="devis__container-fourth-block-table-body-line-total-tva" key={12}>
-                    <input className="devis__container-fourth-block-table-body-line-total-tva-input" type="number" readOnly="readonly" key={13}/>
+                <td className="devis__container-section-fourth-block-table-body-line-total-tva" key={12}>
+                    <input className="devis__container-section-fourth-block-table-body-line-total-tva-input" type="number" readOnly="readonly" key={13}/>
                 </td>
-                <td className="devis__container-fourth-block-table-body-line-total" key={14}>
-                    <input className="devis__container-fourth-block-table-body-line-total-input" type="number" readOnly="readonly" key={15}/>
+                <td className="devis__container-section-fourth-block-table-body-line-total" key={14}>
+                    <input className="devis__container-section-fourth-block-table-body-line-total-input" type="number" readOnly="readonly" key={15}/>
                 </td>
             </tr>
         </>
