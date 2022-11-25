7340ef333c7d9cbf35e0c52280a187b347558c8c
XFGXTueur <tueurxfgx@gmail.com>
2022-11-25 18:32:34 +0100

    fix: readonly to readOnly

--- src/Pages/Devis/Item.jsx
+++ src/Pages/Devis/Item.jsx
@@ -17,7 +17,7 @@ const Item = () => {
                     <input className="devis__container-fourth-block-table-body-line-unitPrice-input" type="number" key={8}/>
                 </td>
                 <td className="devis__container-fourth-block-table-body-line-tva" key={9}>
-                    <p className="devis__container-fourth-block-table-body-line-tva-text" type="number" readonly="readonly" key={10}/>
+                    <p className="devis__container-fourth-block-table-body-line-tva-text" type="number" readOnly="readonly" key={10}/>
                     <select className="devis__container-fourth-block-table-body-line-tva-value" name="tva" id="tva" key={11}>
                             <option value="0">0%</option>
                             <option value="6">6%</option>
@@ -26,10 +26,10 @@ const Item = () => {
                     </select>
                 </td>
                 <td className="devis__container-fourth-block-table-body-line-total-tva" key={12}>
-                    <input className="devis__container-fourth-block-table-body-line-total-tva-input" type="number" readonly="readonly" key={13}/>
+                    <input className="devis__container-fourth-block-table-body-line-total-tva-input" type="number" readOnly="readonly" key={13}/>
                 </td>
                 <td className="devis__container-fourth-block-table-body-line-total" key={14}>
-                    <input className="devis__container-fourth-block-table-body-line-total-input" type="number" readonly="readonly" key={15}/>
+                    <input className="devis__container-fourth-block-table-body-line-total-input" type="number" readOnly="readonly" key={15}/>
                 </td>
             </tr>
         </>
