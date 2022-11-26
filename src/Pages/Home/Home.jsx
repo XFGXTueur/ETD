226cb5fea3fa8b25651cc9bdaae39fc23b19e95d
Thomas F <68472970+WalTho@users.noreply.github.com>
2022-11-26 03:15:54 +0100

    feat: translation keys for variabilized text in documents (#22)

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import FR from '../../locales/fr/translation.json';
+
+const t = FR;
 
 const Home = () => {
     return (
         <div className="home">
             <div className="home__container">
+                <h1>{t.test}</h1>
                 <img className="home__container-img" src="./logo192.png" alt="logo"></img>
                 <div className="home__container-btn">
                     <a href="/devis" className="home__container-btn-link" alt="devis">D<span className="home__container-btn-link-bis">evis</span></a>
